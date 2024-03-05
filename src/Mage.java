0f53b4c73bed39de15aa62be6df8bb866e0fe107
Ilampoornan <162051436+Ilampoornan@users.noreply.github.com>
2024-03-05 11:48:13 +0530

    Update Mage.java

--- src/Mage.java
+++ src/Mage.java
@@ -3,3 +3,34 @@ public class Mage extends Character{
         super(name, price, attack, defence, health, speed);
     }
 }
+
+class Warlock extends Mage {
+    Warlock() {
+        super("Warlock", 100, 12, 7, 10, 12);
+    }
+}
+
+class Illusionist extends Mage {
+    Illusionist() {
+        super("Illusionist", 120, 13, 8, 12, 14);
+    }
+}
+
+class Enchanter extends Mage {
+    Enchanter() {
+        super("Enchanter", 160, 16, 10, 13, 16);
+    }
+}
+
+class Conjurer extends Mage {
+    Conjurer() {
+        super("Conjurer", 195, 18, 15, 14, 12);
+    }
+}
+
+class Eldritch extends Mage {
+    Eldritch() {
+        super("Eldritch", 270, 19, 17, 18, 14);
+    }
+}
+
