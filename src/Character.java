41cee78b65a21e5a1497e164ad10831a0b78f85a
Ilampoornan <162051436+Ilampoornan@users.noreply.github.com>
2024-03-07 11:26:32 +0530

    Update Character.java

--- src/Character.java
+++ src/Character.java
@@ -11,13 +11,18 @@ public abstract class Character {
 
     int speed;
 
-    Character(String name,int price,int attack ,int defence,int health,int speed){
+    String category;
+
+
+
+    Character(String name,int price,int attack ,int defence,int health,int speed,String category){
         this.name=name;
         this.price=price;
         this.attack=attack;
         this.defence=defence;
         this.health=health;
         this.speed=speed;
+        this.category=category;
     }
 
 
