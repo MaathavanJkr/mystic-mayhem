4e17542c01e7027bb92b5e3c4e3b913bede5dbc0
Ilampoornan <162051436+Ilampoornan@users.noreply.github.com>
2024-03-07 11:28:42 +0530

    Update Archer.java

--- src/Archer.java
+++ src/Archer.java
@@ -1,36 +1,37 @@
 public class Archer extends Character{
-    Archer(String name, int price, int attack, int defence, int health, int speed) {
-        super(name, price, attack, defence, health, speed);
+    Archer(String name, int price, int attack, int defence, int health, int speed,String category) {
+        super(name, price, attack, defence, health, speed,category);
     }
 }
 
 class Shooter extends Archer {
     Shooter() {
-        super("Shooter", 80, 11, 4, 6, 9);
+        super("Shooter", 80, 11, 4, 6, 9,"Highlanders");
     }
 }
 
 class Ranger extends Archer {
     Ranger() {
-        super("Ranger", 115, 14, 5, 8, 10);
+        super("Ranger", 115, 14, 5, 8, 10,"Highlanders" +
+                "");
     }
 }
 
 class Sunfire extends Archer {
     Sunfire() {
-        super("Sunfire", 160, 15, 5, 7, 14);
+        super("Sunfire", 160, 15, 5, 7, 14,"Sunchildren");
     }
 }
 
 class Zing extends Archer {
     Zing() {
-        super("Zing", 200, 16, 9, 11, 14);
+        super("Zing", 200, 16, 9, 11, 14,"Sunchildren");
     }
 }
 
 class Saggitarius extends Archer {
     Saggitarius() {
-        super("Saggitarius", 230, 18, 7, 12, 17);
+        super("Saggitarius", 230, 18, 7, 12, 17,"Mystics");
     }
 }
 
