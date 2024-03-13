a5bc18e469df2c12e6d83d2fb7eb8bbe5eca1eeb
unknown <ilampooornan@gmail.com>
2024-03-13 20:22:17 +0530

    pulling from main

--- src/Homeground.java
+++ src/Homeground.java
@@ -1,36 +1,27 @@
 public class Homeground {
-    String name;
-
-    public Homeground(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
 }
 
 //extend Homeground for subclasses Hillcrest, Marshland, Desert and Arcane
 class Hillcrest extends Homeground {
     Hillcrest() {
-        super("Hillcrest");
+        super();
     }
 }
 
 class Marshland extends Homeground {
     Marshland() {
-        super("Marshland");
+        super();
     }
 }
 
 class Desert extends Homeground {
     Desert() {
-        super("Desert");
+        super();
     }
 }
 
 class Arcane extends Homeground {
     Arcane() {
-        super("Arcane");
+        super();
     }
 }
